836c8ee2b078379f5c3f6dbf84f244648eb0c4b8
jianghang <hangscer@gmail.com>
2017-12-29 13:22:13 +0800

    增加user节点显示

--- src/main/scala/akka/actor/monitor/AkkaMonitorActor.scala
+++ src/main/scala/akka/actor/monitor/AkkaMonitorActor.scala
@@ -18,19 +18,30 @@ class AkkaMonitorActor extends WrapperActor {
   override def wrapReceive: Receive = {
     case FetchChildrenListResult(actor, childrenList) => //获取该actor的直接子节点，形成树
       tree = childrenList.foldRight(tree) { (ch, _tree) =>
-        _tree.insertChild(_ == actor.path)(ch.path)
+        actor.path.parent.name == "user" match {
+          case true =>
+            var tempTree = _tree
+            tempTree = tempTree.insertChild(_ == actor.path.parent)(actor.path) //已经存在的节点不会插入
+            tempTree = tempTree.insertChild(_ == actor.path)(ch.path)
+            tempTree
+          case false =>
+            var tempTree = _tree
+            tempTree = tempTree.insertChild(_ == actor.path)(ch.path)
+            tempTree
+        }
       }
       childrenList.foreach(ch => ch ! FetchChildren)
+      tree.print()
     case FetchActorRefResult(actor) => //第一步获取root节点下的 所有直接子节点
       tree = tree match {
         case Leaf(root) =>
-          Branch(root, List(Leaf(actor.path)))
+          Branch(root, List(Branch(actor.path.parent, List(Leaf(actor.path))))) //root -> user(someNode.parent 虚拟节点) -> someNode
         case Branch(root, bs) =>
-          Branch(root, Leaf(actor.path) :: bs)
+          Branch(root, bs.map(t => t.insertChild(_ == actor.path.parent)(actor.path)))
       }
       actor ! FetchChildren
     case FetchActorStarted(actor) => //监控每一个actor的创建
-      tree = tree.insertChild(_ == actor.path.parent)(actor.path)
+    //      tree = tree.insertChild(_ == actor.path.parent)(actor.path)
     case FetchActorTerminated(actor) => //监控每一个actor的销毁
       tree = tree.deleteSubTree(_ == actor.path)
   }
--- src/main/scala/nathan/Main.scala
+++ src/main/scala/nathan/Main.scala
@@ -2,12 +2,8 @@ package nathan
 
 import akka.actor.baseActor._
 import akka.actor.monitor.AkkaMonitorActor
-import akka.actor.{ActorRef, ActorSystem, Props}
-
-import scala.concurrent.Await
-import akka.pattern._
+import akka.actor.{ActorSystem, Props}
 import akka.util.Timeout
-import nathan.tree.{Branch, Leaf, Tree}
 
 import scala.concurrent.duration._
 
@@ -15,25 +11,15 @@ object Main extends App {
   implicit val timeout = Timeout(13 seconds)
   val system = ActorSystem("akkaMonitor")
   val genActor = system.actorOf(Props(classOf[GenActor], 4))
+  println(genActor.path.parent.name) // user
+  println(genActor.path.root)
   system.actorOf(Props[AkkaMonitorActor], "monitorCenter")
-
-  def getChildrenList(actorRef: ActorRef): FetchChildrenListResult = {
-    Await.result((actorRef ? FetchChildren).mapTo[FetchChildrenListResult], 10 seconds)
-  }
-
-  def genTree(actorRef: ActorRef): Tree[ActorRef] = getChildrenList(actorRef).childrenList match {
-    case Nil =>
-      Leaf(actorRef)
-    case children =>
-      Branch(actorRef, children.map(ch => genTree(ch)))
-  }
 }
 
 class GenActor(n: Int) extends WrapperActor {
   override def wrapPreStart(): Unit = n match {
     case 0 =>
     case _ => (1 to n).foreach { _ =>
-      Thread.sleep(1000)
       context.actorOf(Props(classOf[GenActor], n - 1))
     }
   }
